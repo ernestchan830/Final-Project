f46fe3c357cdde5675f92245cd6a3897009d1703
student <ernestchan830@gmail.com>
2019-07-23 13:32:39 -0700

    Timer2

--- app/src/main/java/ernestchan830/example/sleepapp/SleepFragment.java
+++ app/src/main/java/ernestchan830/example/sleepapp/SleepFragment.java
@@ -1,5 +1,6 @@
 package ernestchan830.example.sleepapp;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.SystemClock;
@@ -8,24 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SleepFragment extends Fragment {
+public class SleepFragment extends Fragment implements View.OnClickListener {
 
     private Chronometer stopwatch;
     private boolean running;
     private long pauseOffset;
+    private ImageButton mSleepButton;
 
-//    @Override
-//    protected void onCreate(Bundle savedInstances) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        stopwatch = findViewById(R.id.stopwatch);
-//    }
 
     public void startStopwatch(View v) {
         if(!running)  {
@@ -36,17 +32,25 @@ public class SleepFragment extends Fragment {
     }
 
     public void stopStopwatch(View v) {
-//        if(running)  {
-//            stopwatch.stop();
-//            pauseOffset = SystemClock.elapsedRealtime() - stopwatch.getBase();
-        stopwatch.setBase(SystemClock.elapsedRealtime());
-        pauseOffset = 0;
+        if (running) {
+            stopwatch.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - stopwatch.getBase();
+            stopwatch.setBase(SystemClock.elapsedRealtime());
+            pauseOffset = 0;
         }
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sleep, null);
+        View view = inflater.inflate(R.layout.fragment_sleep, null);
+        mSleepButton = (ImageButton) view.findViewById(R.id.sleep_button);
+        mSleepButton.setOnClickListener(this);
 
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
